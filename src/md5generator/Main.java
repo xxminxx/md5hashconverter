d5247db5fa90c7f550e1c599426db5e999e5a608
Shermin <xxsherminkudo@gmail.com>
2017-07-09 16:19:59 +0800

    Add files via upload

---
+++ src/md5generator/Main.java
@@ -0,0 +1,13 @@
+package md5generator;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        md5 form = new md5();
+        form.setVisible(true);
+    }
+    
+}
